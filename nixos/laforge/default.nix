ae13cf5e6f67c141b6f2aefc586c900aff0b18d8
Adam Stuart <astuart@fkinls.com>
2024-07-21 16:21:45 -0400

    update laforge

--- nixos/laforge/default.nix
+++ nixos/laforge/default.nix
@@ -104,6 +104,6 @@
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
